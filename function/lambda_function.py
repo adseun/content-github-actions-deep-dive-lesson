51dd0603fd4d885a9e8b554bdc71e0214a6e33c3
Fuad Adeshina <100579559+adseun@users.noreply.github.com>
2024-12-05 01:20:45 +0100

    Update lambda_function.py

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -13,7 +13,10 @@ def lambda_handler(event, context):
     if event["input"] == "Hello":
 
         return "World"
+    if event["input"] == "Hi":
+
+        return "Omo"
 
     else:
 
-        raise
+        raise 
